ad470a4b48dee29c3e8d723c89ea69a0d4de9554
Serg <ksyssau@ya.ru>
2023-06-07 20:28:49 +0300

    Шаг 3.2

--- src/main/java/pro/sky/calc/CalcController.java
+++ src/main/java/pro/sky/calc/CalcController.java
@@ -1,14 +1,21 @@
 package pro.sky.calc;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalcController {
 
 @GetMapping(path="/calculator")
-public String hello() {
+public String welcome() {
     return "Добро пожаловать в калькулятор!";
+}
+@GetMapping(path= "/calculator/plus")
+public String plus(@RequestParam("num1") int num1,@RequestParam("num2") int num2) {
+
+    return num1 + "+" + num2 + "=" + (num1+num2);
+
 }
 
 }
