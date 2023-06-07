17bee84344cd3fba178671a272ddcc166c0ffe8b
Serg <ksyssau@ya.ru>
2023-06-07 20:47:15 +0300

    Шаг 3.3

--- src/main/java/pro/sky/calc/CalcController.java
+++ src/main/java/pro/sky/calc/CalcController.java
@@ -1,25 +1,42 @@
 package pro.sky.calc;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalcController {
 
-@GetMapping(path="/calculator")
-public String welcome() {
-    return "Добро пожаловать в калькулятор!";
-}
-@GetMapping(path= "/calculator/plus")
-public String plus(@RequestParam("num1") int num1,@RequestParam("num2") int num2) {
+    @GetMapping()
+    public String welcome() {
+        return "Добро пожаловать в калькулятор!";
+    }
 
-    return num1 + "+" + num2 + "=" + (num1 + num2);
-}
-@GetMapping(path= "/calculator/minus")
-public String minus(@RequestParam("num1") int num1,@RequestParam("num2") int num2) {
+    @GetMapping(path = "/plus")
+    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
 
-    return num1 + "-" + num2 + "=" + (num1 - num2);
-}
+        return num1 + "+" + num2 + "=" + (num1 + num2);
+    }
+
+    @GetMapping(path = "/minus")
+    public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+
+        return num1 + "-" + num2 + "=" + (num1 - num2);
+    }
+
+    @GetMapping(path = "/multiply")
+    public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+
+        return num1 + "*" + num2 + "=" + (num1 * num2);
+    }
+
+    @GetMapping(path = "/divide")
+    public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        if (num2 == 0) {
+            return "Ошибка, елить на ноль нельзя";
+        } else return num1 + "/" + num2 + "=" + (num1 / num2);
+            }
 
 }
